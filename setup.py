c38b6b0ef10c59248fe6fd55e447e0a9c6b59dd4
brenden.dielissen <brenden.dielissen@ucalgary.ca>
2021-03-10 17:26:18 -0700

    - initial commit... again

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DAD Python Package',
+    version='1.0.0',
+    packages=['dad', 'dad.sensors'],
+    url='https://github.com/BrendenDielissen-uofc/UofC-DAD',
+    license='',
+    author='brenden.dielissen',
+    author_email='brenden.dielissen@ucalgary.ca',
+    description='Python package for use with DAD device.'
+)
