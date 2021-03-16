dd47430afa338373ffef596de5eba0a34082247d
brenden.dielissen <brenden.dielissen@ucalgary.ca>
2021-03-16 12:02:51 -0600

    Merging Ryan branch with master: - moving changes to iio_sensors.py to lps331ap.py - deleting iio_sensors.py

--- dad/sensors/iio/lps331ap.py
+++ dad/sensors/iio/lps331ap.py
@@ -1,5 +1,7 @@
 # DAD Packages
 import dad.sensors.iio.iio_sensor.IIOSensor
+
+
 # Python Packages
 
 
@@ -11,21 +13,28 @@ class LPS331AP(IIOSensor):
         https://www.st.com/en/mems-and-sensors/lps331ap.html#:~:text=The%20LPS331AP%20is%20available%20in,to%20reach%20the%20sensing%20element.
     """
 
-    def __init__(self):
+    def __init__(self, device, channel_names):
         super().__init__('lps331ap', ['pressure', 'temp'])
 
     def get_temperature(self):
         """ Gets the current internal temperature in Celcius.
-
         Returns:
             (double): The current internal temperature reading.
         """
-        pass
+        chan = self.get_channel('temp')
+        raw_temp = float(chan.attrs['raw'].value)
+        offset = float(chan.attrs['offset'].value)
+        scale = float(chan.attrs['scale'].value)
+        temp = (offset + raw_temp) * (scale / 1000)
+        return temp
 
     def get_pressure(self):
         """ Gets the current internal pressure reading in millibars.
-
         Returns:
             (double): The current internal pressure reading.
         """
-        pass
+        chan = self.get_channel('pressure')
+        raw_pressure = float(chan.attrs['raw'].value)
+        scale = float(chan.attrs['scale'].value)
+        pressure = (raw_pressure * scale) / 100
+        return pressure
